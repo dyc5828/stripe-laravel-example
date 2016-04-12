c76a232a847d50818da3d74eba1fe7409311970c
Daniel Chen <dyc5828@gmail.com>
2016-04-12 12:10:09 -0700

    basic templates

---
+++ resources/views/header.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-default">
+	<div class="container">
+
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-items">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="/"><strong>AwesomeShop</strong></a>
+		</div><!--/.navbar-header-->
+
+		<div id="nav-items" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a href="/about">
+						Your Cart
+					</a>
+				</li>
+			</ul>
+		</div><!--/#nav-items-->
+
+	</div><!--/.container-->
+</nav>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<title>@yield('page-title')</title>
+
+<!--CSS-->
+<!--bootstrap css-->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<!--page specific css-->
+@yield('page-css')
+
+<!--JS-->
+<!--jQuery-->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<!--bootstrap js REQUIRE jq-->
+<script defer src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<!--page specific js-->
+@yield('page-js')
+
+</head>
+<body>
+<!--HEADER-->
+@include('header')
+<!--CONTENT-->
+@yield('content')
+</body>
+</html>
