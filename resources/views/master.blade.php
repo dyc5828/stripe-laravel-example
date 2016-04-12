2b779c97f8571d5b1e90a14e80c614516f6611b1
Daniel Chen <dyc5828@gmail.com>
2016-04-12 12:56:53 -0700

    updated bootstrap and jquery

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,26 +4,36 @@
 <meta charset="UTF-8">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>@yield('title') | AwesomeShop</title>
-
 <!--CSS-->
 <!--bootstrap css-->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link
+	rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+	integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+	crossorigin="anonymous"
+>
 <!--page specific css-->
 @yield('css')
-
-<!--JS-->
-<!--jQuery-->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<!--bootstrap js REQUIRE jq-->
-<script defer src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<!--page specific js-->
-@yield('js')
-
 </head>
 <body>
 <!--HEADER-->
 @include('header')
 <!--CONTENT-->
 @yield('content')
+<!--JS-->
+<!--jQuery-->
+<script
+	src="//code.jquery.com/jquery-2.2.3.js"
+	integrity="sha256-laXWtGydpwqJ8JA+X9x2miwmaiKhn8tVmOVEigRNtP4="
+	crossorigin="anonymous"
+></script>
+<!--bootstrap js-->
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+	integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+	crossorigin="anonymous"
+></script>
+<!--page specific js-->
+@yield('js')
 </body>
 </html>
