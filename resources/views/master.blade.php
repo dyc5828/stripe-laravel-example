19447144968ec1e7ca107854c729a48ad4a641b2
Daniel Chen <dyc5828@gmail.com>
2016-04-12 13:01:17 -0700

    updated master template for bootstrap compat and content

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>@yield('title') | AwesomeShop</title>
 <!--CSS-->
@@ -19,7 +21,9 @@
 <!--HEADER-->
 @include('header')
 <!--CONTENT-->
-@yield('content')
+<div class="container">
+	@yield('content')
+</div>
 <!--JS-->
 <!--jQuery-->
 <script
