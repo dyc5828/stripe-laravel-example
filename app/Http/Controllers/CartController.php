455a573e4ffcf81fe2a0d67f474fe3e528398cc2
Daniel Chen <dyc5828@gmail.com>
2016-04-12 18:36:11 -0700

    starter

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -39,28 +39,7 @@ class CartController extends Controller {
 	}
 
 	public function charge(Request $request) {
-		if (!$request->input('stripeToken')) {
-			return view('error');
-		}
-		$error = null;
 
-		try {
-			\Stripe\Stripe::setApiKey("sk_test_3N2t85bHxsrQlU4EKfSNQZzZ");
-
-			$charge = \Stripe\Charge::create([
-				'amount' => $request->input('price') * 100,
-				'currency' => 'usd',
-				'source' => $request->input('stripeToken'),
-			]);
-		} catch (Exception $e) {
-			$error = $e->getMessage();
-		}
-
-		if (!$error) {
-			return dd($charge);
-		}
-		return dd($error);
-		// return view('error');
 	}
 
 	public function review() {
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,5 @@
 <?php
 
 Route::group(['middleware' => ['web']], function () {
-    Route::get('/cart', 'CartController@index');
-    Route::post('/charge', 'CartController@charge');
-    // Route::get('/charge', 'CartController@charge');
-    Route::get('/order', 'CartController@review');
+
 });
